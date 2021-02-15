9b5d62b3b1c7f4f507c4badcaa37cc812717f188
M.Andrew <skrembler@list.ru>
2021-02-16 09:53:26 +1000

    done!!!!!

--- avito.py
+++ avito.py
@@ -1,31 +1,27 @@
 from selenium import webdriver
-import pickle
-import time
+
 
 browser = webdriver.Firefox()
 browser.get("https://www.avito.ru/sochi/lichnye_veschi?cd=1&d=1")
-
-# for cookie in pickle.load(open('session','rb')):
-#     browser.add_cookie(cookie)
-# browser.refresh()
-
 sign_in_and_registration_button = browser.find_element_by_xpath("/html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/a")
 sign_in_and_registration_button.click()
 login_field = browser.find_element_by_xpath("/html/body/div[7]/div/div/div/div/span/div[1]/div[1]/form/div[3]/div/div[1]/div/div/div/label/input")
-login_field.send_keys("skrembler@list.ru")
+login_field.send_keys("mymail@mail.ru")
 password_field = browser.find_element_by_xpath("/html/body/div[7]/div/div/div/div/span/div[1]/div[1]/form/div[3]/div/div[2]/div/div/div/label/input")
-password_field.send_keys("S0_8MDF_AKam")
+password_field.send_keys("hardpassword")
 sign_in_button = browser.find_element_by_xpath("/html/body/div[7]/div/div/div/div/span/div[1]/div[1]/form/div[5]/div/div/div/div/div/button")
 sign_in_button.click()
-time.sleep(60)
 goods = browser.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/div[3]/div[3]/div[1]/div[4]/div[1]/div[2]/div[1]/a")
 new_page = goods.get_attribute("href")
 browser.get(new_page)
 buy_with_delivery_button =  browser.find_element_by_xpath("/html/body/div[3]/div[1]/div[3]/div[4]/div[2]/div[1]/div/div[2]/div/div[1]/div/span/span/button")
 buy_with_delivery_button.click()
-phone_filed = browser.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/div[1]/div[2]/form/div[2]/div/div[3]/div/div[2]/div[1]/label/input")
-print("!!!!!!!!!!!!!!!!",phone_filed.text)
+phone_field = browser.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/div[1]/div[2]/form/div[2]/div/div[3]/div/div[2]/div[1]/label/input")
+phone_number = phone_field.get_attribute("value")
+if phone_number == "":
+    print("passed")
+else:
+    print("fail")
 
-# pickle.dump(browser.get_cookies(),open('session', 'wb'))
 
 
